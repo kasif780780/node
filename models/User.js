a4bb9156dd37fe949ca9cc9b178693629c94b984
kasif <kasif780780@gmail.com>
2018-10-08 21:00:51 +0530

    Add User Shema in model folder

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create SChema 
+
+const UserSchema=new Schema({
+ name:{
+     type:string,
+     required:true
+ },
+ 
+email:{
+    type:string,
+    required:true
+},
+password:{
+    type:string,
+    required:true
+},
+avatar:{
+    type:string,
+    required:true
+},
+date:{
+    type:Date,
+    default:Date.now
+},
+});
+
+module.exports = User = mongoose.model('users',UserSchema);
